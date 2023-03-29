b81f30df23306852e54ef12ce120620a4b61be0c
Siddhant <84025779+methlox@users.noreply.github.com>
2023-03-29 08:38:54 +0530

    added input fields, buttons, tried adding auth-failed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
+import { ClerkProvider } from "@clerk/nextjs";
 import { type AppType } from "next/dist/shared/lib/utils";
 
 import "npm/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+  <ClerkProvider {...pageProps} >
+    <Component {...pageProps} />
+  </ClerkProvider>) 
+  ;
 };
 
 export default MyApp;
